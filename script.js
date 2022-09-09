26a0644dcd58090f8e23ed8c8bb1ed08b8d68e51
omerome83 <46061735+omerome83@users.noreply.github.com>
2022-09-08 23:44:20 -0400

    Added font-awesome element variables

---
+++ script.js
@@ -0,0 +1,3 @@
+const faLiElement = `<span class="fa-li">`;
+const faSolidCheckSquareBullet = `<i class="fa-solid fa-check-square"></i>`;
+const faRegularSquareBullet = `<i class="fa-regular fa-square"></i>`;
