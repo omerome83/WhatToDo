aaffa120056c916a76de2878417f3117cd608591
omerome83 <46061735+omerome83@users.noreply.github.com>
2022-09-09 01:43:57 -0400

    Added functionality for adding and removing todos

--- script.js
+++ script.js
@@ -3,22 +3,51 @@ const todoText = document.querySelector(".todo-text");
 const addTodo = document.querySelector(".add-todo");
 const removeTodo = document.querySelector(".remove-todo");
 
-const faLiElement = `<span class="fa-li">`;
-const faSolidCheckSquareBullet = `<i class="fa-solid fa-check-square"></i>`;
-const faRegularSquareBullet = `<i class="fa-regular fa-square"></i>`;
+const ul = document.querySelector(".fa-ul");
+
+// const faLiClassName = `<span class="fa-li">`;
+// const faSolidCheckSquareBullet = `<i class="fa-solid fa-check-square"></i>`;
+// const faRegularSquareBullet = `<i class="fa-regular fa-square"></i>`;
 
 todoText.addEventListener("keypress", (e) => {
   if (e.key === "Enter" && todoText.value !== "") {
-    console.log(todoText.value);
-    todoText.value = "";
+    addNewTodo(todoText.value);
+    value = todoText.value = "";
   }
 });
 
 addTodo.addEventListener("click", (e) => {
   if (todoText.value !== "") {
-    console.log(todoText.value);
-    todoText.value = "";
+    addNewTodo(todoText.value);
+    value = todoText.value = "";
   }
 });
 
-removeTodo.addEventListener("click", (e) => {});
+removeTodo.addEventListener("click", (e) => {
+  removeTodos();
+});
+
+const addNewTodo = (text) => {
+  //   const li = document.createElement("li");
+  //   const span = document.createElement("span");
+
+  //   span.setAttribute("class", faLiClassName);
+  //   li.classList.add("fa-li");
+  //   span.classList.add("fa-li");
+  //   let text = document.createTextNode(text);
+  //   li.appendChild(span);
+  //   li.appendChild(text);
+  //   ul.appendChild(li);
+
+  let html = "";
+
+  html = `
+    <li><span class="fa-li"><i class="fa-regular fa-square"></i></span>${text}</li>
+`;
+
+  ul.innerHTML += html;
+};
+
+const removeTodos = () => {
+  ul.innerHTML = "";
+};
