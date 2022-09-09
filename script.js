45f7f71ab1c04753b0ffd5433913ebf99b980002
omerome83 <46061735+omerome83@users.noreply.github.com>
2022-09-08 23:55:01 -0400

    Added eventlisteners for the buttons and textbox

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const card = document.querySelector(".todo-card");
+const todoText = document.querySelector(".todo-text");
+const addTodo = document.querySelector(".add-todo");
+const removeTodo = document.querySelector(".remove-todo");
+
 const faLiElement = `<span class="fa-li">`;
 const faSolidCheckSquareBullet = `<i class="fa-solid fa-check-square"></i>`;
 const faRegularSquareBullet = `<i class="fa-regular fa-square"></i>`;
+
+todoText.addEventListener("keypress", (e) => {
+  if (e.key === "Enter" && todoText.value !== "") {
+    console.log(todoText.value);
+    todoText.value = "";
+  }
+});
+
+addTodo.addEventListener("click", (e) => {
+  if (todoText.value !== "") {
+    console.log(todoText.value);
+    todoText.value = "";
+  }
+});
+
+removeTodo.addEventListener("click", (e) => {});
