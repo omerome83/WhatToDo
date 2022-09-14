e233c946790cf5c8dc8e95af81ce77c1ccf87f45
omerome83 <46061735+omerome83@users.noreply.github.com>
2022-09-13 21:11:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const addTodo = document.querySelector(".add-todo");
 const removeTodo = document.querySelector(".remove-todo");
 
 const ul = document.querySelector(".fa-ul");
+const nodeList = document.getElementsByTagName("LI");
+
+// const faLiClassName = document.getElementById("span fa-li i");
 
 // const faLiClassName = `<span class="fa-li">`;
 // const faSolidCheckSquareBullet = `<i class="fa-solid fa-check-square"></i>`;
@@ -17,9 +20,27 @@ todoText.addEventListener("keypress", (e) => {
 });
 
 addTodo.addEventListener("click", (e) => {
-  if (todoText.value !== "") {
-    addNewTodo(todoText.value);
-    value = todoText.value = "";
+  try {
+    if (todoText.value !== "") {
+      addNewTodo(todoText.value);
+      value = todoText.value = "";
+
+      //   faLiClassName.addEventListener("click", (e) => {
+      //     console.log(e.target);
+      //     console.log("HI");
+      //     // faLiClassName.style.textDecoration = "line-through";
+      //     // if(faLiClassName.classList.contains("fa-regular fa-square") {
+      //     //     faLiClassName.classList.remove("fa-regular fa-square");
+      //     //     faLiClassName.classList.add("fa-solid fa-check-square");
+      //     // })
+      //     // faLiClassName.classList.remove("fa-solid fa-check-square");
+
+      //     // faLiClassName.classList.remove("fa-regular fa-square");
+      //     // faLiClassName.classList.add("fa-solid fa-check-square");
+      //   });
+    }
+  } catch (err) {
+    console.log(err);
   }
 });
 
@@ -41,11 +62,23 @@ const addNewTodo = (text) => {
 
   let html = "";
 
+  //   html = `
+  //     <li><span class="fa-li"><i class="fa-regular fa-square"></i></span>${text}</li>
+  // `;
+
   html = `
-    <li><span class="fa-li"><i class="fa-regular fa-square"></i></span>${text}</li>
+  <li>${text}</li>
 `;
 
   ul.innerHTML += html;
+
+  ul.addEventListener("click", (e) => {
+    if (e.target.tagName === "LI") {
+      console.log(e.target);
+      e.target.style.textDecoration = "line-through";
+      //   e.target.classList.add("fa-regular", "fa-checked");
+    }
+  });
 };
 
 const removeTodos = () => {
