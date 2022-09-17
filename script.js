a2583ceeddcf319607988f7a9018a812697bc2f1
omerome83 <46061735+omerome83@users.noreply.github.com>
2022-09-17 16:04:37 -0400

    Changed the list item to pencil and removed old commented code

--- script.js
+++ script.js
@@ -6,12 +6,6 @@ const removeTodo = document.querySelector(".remove-todo");
 const ul = document.querySelector(".fa-ul");
 const nodeList = document.getElementsByTagName("LI");
 
-// const faLiClassName = document.getElementById("span fa-li i");
-
-// const faLiClassName = `<span class="fa-li">`;
-// const faSolidCheckSquareBullet = `<i class="fa-solid fa-check-square"></i>`;
-// const faRegularSquareBullet = `<i class="fa-regular fa-square"></i>`;
-
 todoText.addEventListener("keypress", (e) => {
   if (e.key === "Enter" && todoText.value !== "") {
     addNewTodo(todoText.value);
@@ -38,7 +32,7 @@ const addNewTodo = (text) => {
   let html = "";
 
   html = `
-      <li><span class="fa-li"><i class="fa-solid fa-circle"></i></span>${text}</li>
+      <li><span class="fa-li"><i class="fa-solid fa-pencil"></i></span>${text}</li>
   `;
 
   ul.innerHTML += html;
